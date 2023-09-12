0b7a4c1be12fe66a99a7793780f7b5a1ccfa0e32
KhizarAlam20 <khizaralam20@gmail.com>
2023-09-11 22:32:52 -0700

    insetion sort

---
+++ insertionSort.java
@@ -0,0 +1,30 @@
+package data_structure_practice_question;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class insertionSort {
+    public static void main(String[]args){
+
+        int arr[]={4,12,11,20};
+
+        int n = arr.length;
+
+        for(int i = 1 ; i<n ; i++){
+            int key = arr[i];
+            int j = i-1;
+            for(;j>=0;j--){
+                if(arr[j]<key){
+                    arr[j+1]=arr[j];
+                }else{
+                    break;
+                }
+            }
+            arr[j+1]=key;
+        }
+
+        System.out.println("Print after Array : ");
+        for (int i = 0; i <n ; i++) {
+            System.out.print(arr[i]+"\t");
+        }
+    }
+}
